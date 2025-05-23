628d8b86b6668516e7e1b065e5e09edce8fd46d5
Airyl77 <e.patic82@gmail.com>
2025-05-23 23:54:09 +0300

    Added file for CircleShape class
    Changes to be committed:
    new file:   circleshape.py
    modified:   main.py

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    clock = pygame.time.Clock()
 
     while True:
         for event in pygame.event.get():
@@ -17,6 +19,7 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
